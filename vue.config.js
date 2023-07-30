487f732922d042307f92d1339b92280b6e0cf23b
Khush <ganievhushbaht@yandex.ru>
2023-07-30 03:35:43 +0300

    fix: publichPath

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
-  ublicPath: "/test-project-ReactivePhone/",
+  publicPath: "/test-project-ReactivePhone/",
 });
